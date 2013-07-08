62a2edc4963f1a181b746f0b19664e23c2c41c95
Raphael Randschau <rrandschau@weluse.de>
2013-07-08 14:27:11 +0200

    add typhoeus for requests

--- lib/travanto/client.rb
+++ lib/travanto/client.rb
@@ -1,10 +1,19 @@
+require 'typhoeus'
+
 module Travanto
   class Client
     attr_accessor :username, :password
+    include Typhoeus
 
     def initialize username, password
       @username = username
       @password = password
     end
+
+    remote_defaults :on_success => lambda {|response| JSON.parse(response.body)},
+                    :on_failure => lambda {|response| puts "error code: #{response.code}"},
+                    :base_uri   => "http://api.travanto.de"
+
+    define_remote_method :belegzeiten, :path => '/json/belegzeiten/:object_id'
   end
 end
--- travanto.gemspec
+++ travanto.gemspec
@@ -21,4 +21,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "minitest", "~> 4.7.3"
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+
+  spec.add_dependency "typhoeus", "~> 0.6.3"
 end
