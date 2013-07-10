d41fc8caacdb118dc260b575bf8818e0951b34b5
Raphael Randschau <rrandschau@weluse.de>
2013-07-10 07:58:34 +0200

    send single rental_property occupancies to travanto

--- lib/travanto/client.rb
+++ lib/travanto/client.rb
@@ -22,5 +22,30 @@ module Travanto
 
       return ::Travanto::Response.new(response.success?, response.body)
     end
+
+    def save_occupancies object_id, occupancies
+      request = Typhoeus::Request.new(
+        "http://api.travanto.de/json/belegzeiten/#{object_id}",
+        method: :get,
+        userpwd: "#{username}:#{password}",
+        body: [serialize(object_id, occupancies)]
+      )
+      request.run
+      response = request.response
+
+      return ::Travanto::Response.new(response.success?, response.body)
+    end
+
+    def serialize object_id, occupancies
+      return {
+        objekt: object_id,
+        belegzeiten: occupancies.map { |occupancy|
+          {
+            anreise: occupancy.first,
+            abreise: occupancy.last
+          }
+        }
+      }
+    end
   end
 end
--- test/travanto/client_test.rb
+++ test/travanto/client_test.rb
@@ -30,6 +30,35 @@ class ClientTest < MiniTest::Unit::TestCase
     assert_equal response_body, results.results
   end
 
+  def test_saves_occupancies_to_remote_endpoint
+    response_body = {
+      inserted: "1",
+      deleted: "2"
+    }
+    response = Typhoeus::Response.new(code: 200, body: JSON.dump(response_body))
+    Typhoeus.stub(/travanto/).and_return(response)
+
+    c = ::Travanto::Client.new "user1234", "testtest"
+    c.save_occupancies "1000", [["2013-01-01", "2013-05-01"]]
+
+    assert response.success?
+  end
+
+  def test_generates_proper_serialization
+    serialized = {
+      objekt: "1000",
+      belegzeiten: [
+        {
+          anreise: "2013-01-01",
+          abreise: "2013-05-01"
+        }
+      ]
+    }
+
+    c = ::Travanto::Client.new "user1234", "testtest"
+    assert_equal serialized, c.serialize("1000", [["2013-01-01", "2013-05-01"]])
+  end
+
   def test_returns_empty_resultset_on_remote_errors
     response = Typhoeus::Response.new(code: 401, body: "Nicht authentifiziert.")
     Typhoeus.stub(/travanto/).and_return(response)
